4ebc18334754a1c42ef8df8832e74c2be2803365
fenil292 <65783083+fenil292@users.noreply.github.com>
2021-12-22 16:41:58 +0530

    Delete Assignment-11 directory

--- Assignment-11/Assignment-11/FileManagerService.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-class  FileManagerService {
-    static func getDocDir() -> URL {
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        print("Doc Dir: \(paths[0])")
-        return paths[0]
-    }
-
-}
